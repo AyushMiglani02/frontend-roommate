c33245152500d22eec338a0b81c2a2526c28c8ee
Ayush <ayushmiglani2004@gmail.com>
2024-05-21 22:50:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import './App.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import UserProfile from './PAGES/UserProfile/UserProfile.jsx';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/user/:activepage' element={<UserProfile />} />
+        <Route path="*" element={<div><h1>404 NOT FOUND</h1></div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
