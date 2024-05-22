3524f95d8a6ed1e5b40e6cb562f7efa593efdf60
Harshit Manchanda <128061019+harshit6239@users.noreply.github.com>
2024-05-22 20:48:16 +0530

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import './App.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import UserProfile from './PAGES/UserProfile/UserProfile.jsx';
+import SliderPage from './PAGES/SliderPage/SliderPage.jsx';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<SliderPage />} />
         <Route path='/user/:activepage' element={<UserProfile />} />
-        <Route path="*" element={<div><h1>404 NOT FOUND</h1></div>} />
+        {/* <Route path="/*" element={<div><h1>404 NOT FOUND</h1></div>} /> */}
       </Routes>
     </BrowserRouter>
   );
